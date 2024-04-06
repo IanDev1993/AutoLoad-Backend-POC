63eb0d89aa6d95fd37659e15a6ce2900acdadefd
Ian Dev <ian.dev.1993@gmail.com>
2024-04-06 00:15:41 -0300

    fix: update migrations and config swagger

--- Program.cs
+++ Program.cs
@@ -1,11 +1,35 @@
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+            Title = "Orders API",
+            Version = "v1",
+            Description = "An API to perform Orders operations",
+            TermsOfService = new Uri("https://example.com/terms"),
+            Contact = new OpenApiContact
+            {
+                Name = "Ian Nascimento",
+                Email = "ian.dev.1993@gmail.com",
+                Url = new Uri("https://br.linkedin.com/in/ian-nascimento-3b7574140"),
+            },
+            License = new OpenApiLicense
+            {
+                Name = "Orders API LICX",
+                Url = new Uri("https://example.com/license"),
+            }
+     
+    });
+});
+
 builder.Services.AddDbContext<AppDbContext>();
 
 var app = builder.Build();
@@ -16,23 +40,35 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(config =>
     {
-        config.DocumentTitle = "Orders Api";     
+        
     });
 }
 
 
-app.MapGet("/orders-itens", async (AppDbContext context) => await context.Orders.ToListAsync());
-app.MapGet("/orders-drivers-itens", async (AppDbContext context) => await context.OrderDrivers.ToListAsync());
+app.MapGet("/orders", async (AppDbContext context) => 
+    await context
+            .Orders
+            .Include(x=>x.Driver)
+            .ToListAsync()
+);
 
-app.MapPost("/orders-item", async (Order order, AppDbContext context) =>
+
+app.MapGet("/orders-drivers", async (AppDbContext context) => 
+    await context
+            .OrderDrivers
+            .ToListAsync()
+);
+
+app.MapPost("/orders", async (Order order, AppDbContext context) =>
 {
     context.Orders.Add(order);
     await context.SaveChangesAsync();
 
-    return Results.Created($"/orders-item/{order.Id}", order);
+    return Results.Created($"/orders/{order.Id}", order);
 });
 
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.Run();
 
