cb654134e67545cb899fd5b58290e3c2bfc61860
Ian Dev <ian.dev.1993@gmail.com>
2024-04-07 11:03:40 -0300

    feat: add extesion methods

--- Program.cs
+++ Program.cs
@@ -1,49 +1,16 @@
+using BackEndMimimal.Extensions;
 using BackEndMimimal.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
+
 
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "Orders API",
-        Version="v1",                  
-        Contact = new OpenApiContact
-        {
-            Name = "Ian Nascimento",
-            Email = "ian.dev.1993@gmail.com",
-            Url = new Uri("https://br.linkedin.com/in/ian-nascimento-3b7574140"),
-        }         
-    });
-});
-
-builder.Services.AddDbContext<AppDbContext>();
-builder.Services.AddCors(opt=>opt.AddPolicy("corspolicy",build=> 
-    {
-        build.WithOrigins("*").AllowAnyHeader().AllowAnyMethod();
-    }
-));
+builder.AddArchitectures();
 
 var app = builder.Build();
+app.UseArchitectures();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(config =>
-    {        
-        config.SwaggerEndpoint("/swagger/v1/swagger.json","v1");
-        config.InjectStylesheet("/swagger/custom.css");
-        config.RoutePrefix = string.Empty;
-    });
-}
 
 app.MapGet("/order", async (AppDbContext context) => 
     {
@@ -117,8 +84,6 @@ app.MapDelete("/order", async (
     }
 ).Produces(StatusCodes.Status200OK).Produces(StatusCodes.Status404NotFound).Produces(StatusCodes.Status400BadRequest).WithName("DeleteOrder").WithTags("Order");
 
-app.UseHttpsRedirection();
-app.UseCors("corspolicy");
-app.UseStaticFiles();
+
 app.Run();
 
